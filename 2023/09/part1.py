2e0bbee97ad2033c9dbfecebbdf67a47bb0ebe05
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2023-12-09 08:19:42 +0100

    solver part 1 09

---
+++ 2023/09/part1.py
@@ -0,0 +1,12 @@
+import os
+
+sum = 0
+with open(f"{os.path.dirname(__file__)}/input.txt") as f:
+    for l in f:
+        history = [list(map(int, l.rstrip().split()))]
+        while any(x != 0 for x in history[-1]):
+            history.append([history[-1][i+1] - x for i, x in enumerate(history[-1][:-1])])
+        for i in range(len(history) - 1, 0, -1):
+            history[i-1].append(history[i-1][-1] + history[i][-1])
+        sum += history[0][-1]
+print(sum)
