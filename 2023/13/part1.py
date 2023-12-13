a6e80819872ea1682547a36a2c732398119b5580
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2023-12-13 12:14:48 +0100

    solved part 1 13

---
+++ 2023/13/part1.py
@@ -0,0 +1,23 @@
+import os
+
+blocks = None
+with open(f"{os.path.dirname(__file__)}/input.txt") as f:
+    blocks = [list(map(list, x.strip().split('\n'))) for x in f.read().split('\n\n')]
+
+s = 0
+for block_rows in blocks:
+    block_cols = list(map(list, zip(*block_rows)))
+    for comp_idx, block, multiplier in [(0, block_rows, 100), (len(block_rows) - 1, block_rows, 100), (0, block_cols, 1), (len(block_cols) - 1, block_cols, 1)]:
+        comp_stack = block[comp_idx]
+        for i, stack in enumerate(block):
+            if i == comp_idx: continue
+            mirror_len = abs(comp_idx - i) + 1
+            if mirror_len % 2 != 0 or stack != comp_stack: continue
+            mirror = min(i, comp_idx) + mirror_len // 2
+            if block[min(i, comp_idx):mirror] == block[max(comp_idx, i):mirror-1:-1]:
+                s += mirror * multiplier
+                break
+        else: continue
+        break
+
+print(s)
