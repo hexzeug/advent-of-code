f344b3e6e6c64a96fea1cb700d097f81d734b04c
hexszeug <77232580+hexzeug@users.noreply.github.com>
2024-12-07 12:26:58 +0100

    solved part 2 07

---
+++ 2024/07/part2.hs
@@ -0,0 +1,22 @@
+int :: String -> Int
+int = read
+
+str :: Int -> String
+str = show
+
+parseEquation :: String -> (Int, [Int])
+parseEquation eq =
+    let xs = words eq
+    in (int . init . head $ xs, map int . tail $ xs)
+
+solves :: Int -> [Int] -> Bool
+solves _ [] = False
+solves rs [rs'] = rs == rs'
+solves rs eq =
+    let (a : b : eq') = eq
+    in solves rs ((a + b) : eq') || solves rs ((a * b) : eq') || solves rs (int (str a ++ str b) : eq')
+
+main :: IO ()
+main = do
+    input <- readFile "2024/07/input.txt"
+    print $ sum . map fst . filter (uncurry solves) . map parseEquation . lines $ input
